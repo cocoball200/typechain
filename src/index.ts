bec8004d78fc51d6954f0eb65b0d5751a9d6e20b
mia <cocoball200@gmail.com>
2021-01-09 15:06:14 +0900

    by using class for Object

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
 }
 
-const person = {
-    name: "Mia",
-    age: 100,
-    gender: "female"
-};
+const Mia = new Human("Mia", 100, "female");
 
 const sayHi = (person: Human) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(Mia));
 export { };
